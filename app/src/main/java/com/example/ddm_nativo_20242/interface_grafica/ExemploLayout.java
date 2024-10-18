90803763252f58dfc3846b459884253c571e1579
Gracon Lima <graconlima@yahoo.com.br>
2024-10-18 13:07:00 -0300

    Adicionando Exemplos Iniciais (Ciclo de Vida e Layout)

---
+++ app/src/main/java/com/example/ddm_nativo_20242/interface_grafica/ExemploLayout.java
@@ -0,0 +1,68 @@
+package com.example.ddm_nativo_20242.interface_grafica;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.example.ddm_nativo_20242.R;
+
+public class ExemploLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        //layout com XML
+        setContentView(R.layout.activity_exemplo_layout);
+
+        EditText campoNome = findViewById(R.id.campoNome);
+        EditText campoSenha = findViewById(R.id.campoSenha);
+        Button b = findViewById(R.id.botaoEnviar);
+
+        //Exemplo Eventos
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                autenticacao(campoNome.getText().toString(), campoSenha.getText().toString());
+            }
+        });
+
+        //layout com API Java (simplificado)
+        /*TextView textoNome = new TextView(this);
+        textoNome.setText("Nome:");
+
+        EditText campoNome = new EditText(this);
+
+        TextView textoSenha = new TextView(this);
+        textoSenha.setText("Senha: ");
+
+        MeuEditText campoSenha = new MeuEditText(this, null);
+
+        Button b = new Button(this);
+        b.setText("ENVIAR!");
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                autenticacao(campoNome.getText().toString(), campoSenha.getText().toString());
+            }
+        });
+
+        LinearLayout layoutPrincipal = new LinearLayout(this);
+        layoutPrincipal.addView(textoNome);
+        layoutPrincipal.addView(campoNome);
+        layoutPrincipal.addView(textoSenha);
+        layoutPrincipal.addView(campoSenha);
+        layoutPrincipal.addView(b);
+        setContentView(layoutPrincipal);*/
+
+    }
+    public void autenticacao(String nome, String senha){
+        if(nome.equals("Aluno") && senha.equals("123")){
+            Toast.makeText(this,"Autenticado", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this,"Não Autenticado", Toast.LENGTH_LONG).show();
+        }
+    }
+}
